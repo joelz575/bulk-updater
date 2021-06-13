f44991cd61d7e3e8fe28580a799e7c61b78b442c
joelz575 <joel.harms@mail.mcgill.ca>
2021-06-13 11:31:33 -0400

    minor modification for updating SWAT+ to 60.5.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -45,24 +45,15 @@ public class Main {
             FileOutputStream fos = new FileOutputStream(newFile);
 
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-            /*
-            if(newFile.getName().equals("cbn_zhang2.f90")) {
-                System.out.println(source.getAbsolutePath());
-            }
-            */
+
             while (sourceScanner.hasNextLine()) {
                 String line = sourceScanner.nextLine();
-                /*
-                if(newFile.getName().equals("cbn_zhang2.f90")) {
-                    System.out.println("Printing this line to cbn_zhang2.f90" + line);
-                }
-                */
                 bw.write(line);
                 bw.newLine();
             }
             bw.close();
 
-            //System.out.println("Source files for update: " + source.getName());
+            System.out.println("Source files for update: " + source.getName());
         }
     }
 
